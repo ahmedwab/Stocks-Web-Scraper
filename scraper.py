5778bab238aa0a6776c3643a49f95ca31371aae2
ahmedwab <52334883+ahmedwab@users.noreply.github.com>
2020-12-22 05:02:44 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def list_stocks(soup):
+    list =[]
+    for link in soup.find_all("li")[:-1]:
+        link = link.find("span","column stock-name")
+        list.append(link)
+    return list
+    
+
+
 url="https://money.cnn.com/data/markets/"
 
 html_content = requests.get(url).content
@@ -12,5 +21,4 @@ soup = BeautifulSoup(html_content, "lxml")
 soup = soup.find("ul","module-body wsod most-popular-stocks")
 
 
-for link in soup.find_all("li")[:-1]:
-    print(link.find("span","column stock-name").text)
+print (list_stocks(soup))
